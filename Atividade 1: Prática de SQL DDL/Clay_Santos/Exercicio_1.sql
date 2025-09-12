4e6382fe855bf09a5ea23fa98ae9dfa11638d232
clayviana <clayviana24@gmail.com>
2025-09-11 21:52:30 -0300

    Create Exercicio_1.sql
    11/09/2025

---
+++ Atividade 1: Prática de SQL DDL/Clay_Santos/Exercicio_1.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE loja_de_roupas;
+USE loja_de_roupas;
+CREATE TABLE produtos (	
+			id_produto INT AUTO_INCREMENT PRIMARY KEY ,
+            nome VARCHAR(50) NOT NULL,
+            descricao VARCHAR(255),
+            preco DECIMAL(10, 2) NOT NULL,
+            estoque INT NOT NULL DEFAULT 0
+);
+            
